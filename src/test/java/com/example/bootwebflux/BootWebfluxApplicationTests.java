23483105c442066a2779d9f2276e603624d98008
YX <wdnmdwgzdl666@163.com>
2022-10-18 14:29:36 +0800

    boot-webflux

---
+++ src/test/java/com/example/bootwebflux/BootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
